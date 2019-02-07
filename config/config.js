dfd4eae4c7337608a91e157507bcea6e1b2a6ca7
Brian Chen <brianchen1997@gmail.com>
2019-02-07 02:36:20 -0500

    Fixes

--- config/config.js
+++ config/config.js
@@ -2,27 +2,37 @@ const dotenv = require('dotenv');
 // Setup the configurations to be used by the app
 dotenv.config({ path: './config/.env' });
 
-env_vars = {};
-switch(process.env.NODE_ENV){
-    case 'dev':
-        console.log(1);
-        env_vars =  {
-            port: process.env.DEV_PORT,
-            mysql_host: process.env.DEV_MYSQL_HOST,
-            mysql_user: process.env.DEV_MYSQL_USER,
-            mysql_password: process.env.DEV_MYSQL_PASSWORD,
-            mysql_db: process.env.DEV_MYSQL_DB
-        };
-        break;
-    default:
-        console.log(2);
-        env_vars = {
-            port: process.env.LOCAL_PORT,
-            mysql_host: process.env.LOCAL_MYSQL_HOST,
-            mysql_user: process.env.LOCAL_MYSQL_USER,
-            mysql_password: process.env.LOCAL_MYSQL_PASSWORD,
-            mysql_db: process.env.LOCAL_MYSQL_DB
-        };
-        break;
-}
-module.exports = env_vars;
+// env_vars = {};
+// switch(process.env.NODE_ENV){
+//     case 'dev':
+//         console.log(1);
+//         env_vars =  {
+//             port: process.env.DEV_PORT,
+//             mysql_host: process.env.DEV_MYSQL_HOST,
+//             mysql_user: process.env.DEV_MYSQL_USER,
+//             mysql_password: process.env.DEV_MYSQL_PASSWORD,
+//             mysql_db: process.env.DEV_MYSQL_DB
+//         };
+//         break;
+//     default:
+//         console.log(2);
+//         env_vars = {
+//             port: process.env.LOCAL_PORT,
+//             mysql_host: process.env.LOCAL_MYSQL_HOST,
+//             mysql_user: process.env.LOCAL_MYSQL_USER,
+//             mysql_password: process.env.LOCAL_MYSQL_PASSWORD,
+//             mysql_db: process.env.LOCAL_MYSQL_DB
+//         };
+//         break;
+// }
+// module.exports = env_vars;
+
+module.exports = {
+    port: process.env.PORT,
+    mysql_host: process.env.MYSQL_HOST_LOCAL,
+    mysql_user: process.env.MYSQL_USER,
+    mysql_password: process.env.MYSQL_PASSWORD,
+    mysql_db: process.env.MYSQL_DB,
+    mysql_port: process.env.MYSQL_PORT
+};
+
--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 const mysql = require('mysql');
-const { mysql_host, mysql_user, mysql_password, mysql_db } = require('./config/config');
+const { mysql_host, mysql_user, mysql_password, mysql_db, mysql_port } = require('./config/config');
 
 // Setup connection
 const connection = mysql.createConnection({
     host: mysql_host,
     user: mysql_user,
     password: mysql_password,
+    db_port: mysql_port,
     database: mysql_db
   });
 
 // Connect to DB
 connection.connect((err) => {
     if (err) {
-        return console.error('error: ' + err.message);
+        return console.error('error: ' + err.stack);
     }
     console.log('Connected to the MySQL server.');
 });
